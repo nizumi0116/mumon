2712b3393c781b006b420b61866182cb23ff43d3
nizumi0116 <6219504@ed.tus.ac.jp>
2020-11-07 14:48:31 +0900

    add 2D plot and beam location

--- si_array.cc
+++ si_array.cc
@@ -14,244 +14,301 @@
 #include "math.h"
 #include "TApplication.h"
 
+#include <TStyle.h>
+//#define test
+#ifdef  test
+#include "TROOT.h"
+#endif
+
 using namespace std;
 
 const int CH = 32;
 const int SAMPLE = 2049;
+const TString OPTDIR = "./";
 
 int main(int argc, char *argv[]) {
-  //void si_array(){
+
+  #ifdef test
+  gROOT->SetBatch();
+  #endif
 
   TApplication app("app", 0, 0, 0, 0);
-  TCanvas *c0 = new TCanvas("c0", "c0", 500, 500);
-  
-  //TH1D *pedeHist = new TH1D("h","pedestal:ADCcounts:events",500,2000,2500);
+
+  /*
   TH1D **pedeHist; pedeHist = new TH1D*[CH];
-  for(int p=0;p<CH;p++) {
-    pedeHist[p] = new TH1D(Form("h_%d",p),"pedestal:ADCcounts:events",1000,2000,3000);
-  }
   TH1D **Pedestal; Pedestal = new TH1D*[CH];
-  for(int q=0;q<CH;q++) {
-    Pedestal[q] = new TH1D(Form("h__%d",q),"pedestal:ADCcounts:events",1000,2000,3000);
+  for (int i = 0; i < CH; i++) {
+    pedeHist[i] = new TH1D(Form("h_%d",i), "pedestal:ADCcounts:events", 1000, 2000, 3000);
+    Pedestal[i] = new TH1D(Form("h__%d",i),"pedestal:ADCcounts:events", 1000, 2000, 3000);
   }
-  
-  TString txtname; TString txtname1;
+  */
 
-  TH2D* map_all = new TH2D("map_all", "map_all", 8, -12, 12, 8, -12, 12);
-  TF2* g2 = new TF2("g2", "[0]*exp(- pow((x-[1]),2) / (2*pow([2],2)) ) * exp(- pow((y-[3]),2) / (2*pow([4],2)) )", -10, 10, -10, 10); 
-  //"[0]*gaus(x, [1], [2])*gaus(y, [3], [4])", -10, 10, -10, 10);
+  TH2D **map_all = 0;
+  map_all = new TH2D*[2];
+  for (int i = 0; i < 2; i++) map_all[i] = new TH2D(Form("map_all_%d", i), "si array map;x[mm];y[mm]", 8, -12, 12, 8, -12, 12);
+  //TH2D* map_all = new TH2D("map_all", "si array map;x[mm];y[mm]", 8, -12, 12, 8, -12, 12);
+
+  // 2D Gaussian Distribution
+  TF2* g2 = new TF2("g2", "[0]*exp(- pow((x-[1]),2) / (2*pow([2],2)) ) * exp(- pow((y-[3]),2) / (2*pow([4],2)) )", -10, 10, -10, 10);
   g2->SetParameters(1, 0, 1, 0, 1);
+  //
 
-  int ADC_R[CH][SAMPLE]; double pedestal_R[CH]; double RMS_R[CH]; int integral_R[CH]; int ientry_R; 
-  int ADC_L[CH][SAMPLE]; double pedestal_L[CH]; double RMS_L[CH]; int integral_L[CH]; int ientry_L; 
+  if (argc != 3) {
+    cout << "\nusage : " << argv[0] << "DAQPC1 run number & DAQPC2 run number\n";
+    exit(0);
+  }
+  int runNumberDAQ1 = atoi(argv[1]);
+  int runNumberDAQ2 = atoi(argv[2]);
+  //int runNumberDAQ1 = 32, runNumberDAQ2 = 26; // test
+  //cout << "select DAQPC==1== run number > " << flush;
+  //cin  >> runNumberDAQ1;
+  //cout << "select DAQPC==2== run number > " << flush;
+  //cin  >> runNumberDAQ2;
+
+  ifstream inputDAQ1[CH], inputDAQ2[CH];
+  TString txtName;
+  for (int i = 0; i < CH; i++) {
+    txtName.Form("%srun000%d/run000%d_ch%d.txt", OPTDIR.Data(), runNumberDAQ1, runNumberDAQ1, i); // apply your own path
+    inputDAQ1[i].open(txtName);
+    txtName.Form("%srun000%d/run000%d_ch%d.txt", OPTDIR.Data(), runNumberDAQ2, runNumberDAQ2, i);
+    inputDAQ2[i].open(txtName);
+  }
+
+  double xArray_R[CH] = {9,3,6,3,9,6,0,0,-3,-6,-9,-12,-6,-12,-9,-3,-12,-6,-6,-9,-12,-9,-3,-3,0,0,3,6,9,3,9,6}; // mm (view of down stream)
+  double xArray_L[CH] = {-12,-6,-9,-6,-12,-9,-3,-3,0,3,6,9,3,9,6,0,9,3,3,6,9,6,0,0,-3,-3,-6,-9,-12,-6,-12,-9}; // mm edited 12/28/2019
+  double yArray_R[CH] = {0,0,3,6,6,9,3,9,6,9,6,9,3,3,0,0,-3,-3,-9,-6,-9,-12,-6,-12,-3,-9,-12,-9,-12,-6,-6,-3}; // mm
+  double yArray_L[CH] = {0,0,3,6,6,9,3,9,6,9,6,9,3,3,0,0,-3,-3,-9,-6,-9,-12,-6,-12,-3,-9,-12,-9,-12,-6,-6,-3}; // mm edited 12/28/2019
+
+  int ADC_R[CH][SAMPLE]; double pedestal_R[CH]; double RMS_R[CH]; int integral_R[CH]; int ientry_R;
+  int ADC_L[CH][SAMPLE]; double pedestal_L[CH]; double RMS_L[CH]; int integral_L[CH]; int ientry_L;
+
+  TString optFileName = Form("%sprofile_daqpc1_run000%d.root", OPTDIR.Data(), runNumberDAQ1);
+  TFile *fout_R = new TFile(optFileName, "RECREATE");
   TTree *profile_R = new TTree("profile_R","profile_R");
   profile_R->Branch("ADCcount", ADC_R,      "ADCcount[32][2049]/I");
   profile_R->Branch("pedestal", pedestal_R, "pedestal[32]/D");
   profile_R->Branch("RMS"     , RMS_R,      "RMS[32]/D");
   profile_R->Branch("integral", integral_R, "integral[32]/I");
   profile_R->Branch("ientry",   &ientry_R,  "ientry/I");
-    
-  double xArray_R[32] = {9,3,6,3,9,6,0,0,-3,-6,-9,-12,-6,-12,-9,-3,-12,-6,-6,-9,-12,-9,-3,-3,0,0,3,6,9,3,9,6}; // mm (view of down stream)
-  double xArray_L[32] = {-12,-6,-9,-6,-12,-9,-3,-3,0,3,6,9,3,9,6,0,9,3,3,6,9,6,0,0,-3,-3,-6,-9,-12,-6,-12,-9}; // mm edited 12/28/2019
-  double yArray_R[32] = {0,0,3,6,6,9,3,9,6,9,6,9,3,3,0,0,-3,-3,-9,-6,-9,-12,-6,-12,-3,-9,-12,-9,-12,-6,-6,-3}; // mm
-  double yArray_L[32] = {0,0,3,6,6,9,3,9,6,9,6,9,3,3,0,0,-3,-3,-9,-6,-9,-12,-6,-12,-3,-9,-12,-9,-12,-6,-6,-3}; //edited 12/28/2019
 
-  ifstream fin[CH]; ifstream fin1[CH];
-
-  int runNum; int runNum1;
-  cout << "select DAQPC==1== run number >" << flush; cin >> runNum;
-  cout << "select DAQPC==2== run number >" << flush; cin >> runNum1;
-
-  for(int i=0;i<CH;i++) {
-    txtname.Form("../profile/run000%d/run000%d_ch%d.txt", runNum, runNum, i);
-    fin[i].open(txtname); 
-    txtname1.Form("../profile/run000%d/run000%d_ch%d.txt", runNum1, runNum1, i);
-    fin1[i].open(txtname1);
-  }
-
-  int Sum; int x; int cont=0; int pede; double pedeRMS=0;
+  int sumADC = 0;
+  int x;
+  int pedestalValue = 0;
+  double pedestalRMS = 0;
 
   //daqpc1
-  ientry_R = 0; 
-  while(!fin[0].eof()) {
-    for(int channel=0; channel<CH; channel++) {
-      Sum=0;pede=0;pedeRMS=0;
-      for(int i=0;i<2;i++) {
+  ientry_R = 0;
+  while (!inputDAQ1[0].eof()) {
+    for (int ich = 0; ich < CH; ich++) {
+      sumADC = 0;
+      pedestalValue = 0;
+      pedestalRMS   = 0;
+      for (int i = 0; i < 2; i++) {
 	double dummy;
-	fin[channel] >> dummy;
+	inputDAQ1[ich] >> dummy;
       }
-      for(int isamp=0; isamp<SAMPLE; isamp++) {
-	fin[channel] >> x;
-	ADC_R[channel][isamp] = x;
-	Sum += ADC_R[channel][isamp];
-	if(isamp<100) {
-	  pede =  pede + ADC_R[channel][isamp];
-	  pedeRMS = pedeRMS + pow(ADC_R[channel][isamp],2.0)/100;
+      for (int isample = 0; isample < SAMPLE; isample++) {
+	inputDAQ1[ich] >> x;
+	ADC_R[ich][isample] = x;
+	sumADC += ADC_R[ich][isample];
+	if (isample < 100) {
+	  pedestalValue = pedestalValue + ADC_R[ich][isample];
+	  pedestalRMS   = pedestalRMS + pow(ADC_R[ich][isample], 2.0) / 100;
 	}
       }
-      integral_R[channel] = Sum; 
-      pedestal_R[channel] = pede/100; //dataout(pedestal[channel]);
-      RMS_R[channel] = sqrt(pedeRMS); //dataout(sqrt(pedeRMS));//dataout(RMS[channel]);
+      integral_R[ich] = sumADC;
+      pedestal_R[ich] = pedestalValue / 100;
+      RMS_R     [ich] = sqrt(pedestalRMS);
     }
-    profile_R->Fill(); cont++; ientry_R++;
-    //if(ientry==2000)break; //debug
+    profile_R->Fill();
+    ientry_R++;
   }
 
-  TString ofn;
-  ofn.Form("../profile/profile_daqpc1_run000%d.root", runNum);
-  //TFile er(ofn);
-  //rofile->Write();
-  //er.Close();
-  profile_R->SaveAs(ofn);
-  //profile_R->Write();
-  //tree1->Reset();
+  // Write to TTree & Close
+  profile_R->Write();
+  fout_R->Close();
+  //
 
-  TFile fi(ofn);
   int ADcount_R[CH][SAMPLE]; double peds_R[CH]; double rms_R[CH]; int integ_R[CH]; int ient_R;
-  TTree *tre = (TTree*)fi.Get("profile_R");
-  tre->SetBranchAddress("ADCcount", ADcount_R);
-  tre->SetBranchAddress("pedestal", peds_R);
-  tre->SetBranchAddress("RMS"     , rms_R);
-  tre->SetBranchAddress("integral", integ_R);
-  tre->SetBranchAddress("ientry"  , &ient_R);
+  TFile iptFile_R(optFileName);
+  TTree *tree_R = (TTree*)iptFile_R.Get("profile_R");
+  tree_R->SetBranchAddress("ADCcount", ADcount_R);
+  tree_R->SetBranchAddress("pedestal", peds_R);
+  tree_R->SetBranchAddress("RMS"     , rms_R);
+  tree_R->SetBranchAddress("integral", integ_R);
+  tree_R->SetBranchAddress("ientry"  , &ient_R);
 
-  int nevt = tre->GetEntries();
-  cout << "nevt = " << nevt << "\n";
+  int nevt_R = tree_R->GetEntries();
+  cout << "\nDAQPC1 event = " << nevt_R << "\n\n";
 
-  double realInt[CH][nevt];
-  TH2D *map = new TH2D("map","map",8,-12,12,8,-12,12);
-  TH2D **siArrayHist_R; siArrayHist_R = new TH2D*[nevt];
-  for(int i=0;i<nevt;i++) {
-    siArrayHist_R[i]=new TH2D(Form("h_%d",i),"si array;x [mm];y [mm]",8,-12,12,8,-12,12);
-  }
+  double realInt[CH][nevt_R];
+  TH2D *map_R = new TH2D("map_R", "map_R", 8, -12, 12, 8, -12, 12);
+  TH2D **siArrayHist_R;
+  siArrayHist_R = new TH2D*[nevt_R];
+  for (int i = 0; i < nevt_R; i++) siArrayHist_R[i]=new TH2D(Form("h_%d",i),"si array;x [mm];y [mm]",8,-12,12,8,-12,12);
 
-  for(int evt=0; evt<nevt; evt++) {
-    tre->GetEntry(evt);
-    for(int ich=0; ich<CH; ich++) {
+  for (int evt = 0; evt < nevt_R; evt++) {
+    tree_R->GetEntry(evt);
+    for (int ich = 0; ich < CH; ich++) {
       realInt[ich][evt] = integ_R[ich] - peds_R[ich]*SAMPLE;
-      //dataout(realInt[ich][evt]);
-      if(realInt[ich][evt] <0 ){
-	realInt[ich][evt]=0;
+      if (realInt[ich][evt] < 0) realInt[ich][evt] = 0;
+    }
+    for (int nch = 0; nch < CH; nch++) {
+      for (int s = 0; s < realInt[nch][evt]/nevt_R; s++) { // what does it do?
+	siArrayHist_R[evt]->Fill(xArray_R[nch], yArray_R[nch]);
+	map_R->Add(siArrayHist_R[evt], 1);
+	for (int i = 0; i < 2; i++) map_all[i]->Add(siArrayHist_R[evt], 1);
+	siArrayHist_R[evt]->Reset();
       }
     }
-    for(int nch=0;nch<CH;nch++) {
-      for(int s=0;s<realInt[nch][evt]/nevt;s++) {
-	siArrayHist_R[evt]->Fill(xArray_R[nch],yArray_R[nch]);
-	map->Add(siArrayHist_R[evt],1);
-	map_all->Add(siArrayHist_R[evt], 1);
-	siArrayHist_R[evt]->Reset();
-	}
-    }
   }
-  TH1D *projectX_R = map->ProjectionX("pjx",-12,12);
-  TH1D *projectY_R = map->ProjectionY("pjy",-12,12);            
 
-  TString fileName; fileName.Form("../profile/hoge_%d_%d.root", runNum, runNum1);
-  TFile f1(fileName, "RECREATE");
-  f1.cd();
-  //siArrayHist_R->Write();
-  map->Write();
-  projectX_R->Write();
-  projectY_R->Write();
-  f1.Close();
+  TH1D *projectX_R = map_R->ProjectionX("pjx_R", -12, 12);
+  TH1D *projectY_R = map_R->ProjectionY("pjy_R", -12, 12);
 
-  fi.Close();
-
+  optFileName.Form("%sprofile_daqpc2_run000%d.root", OPTDIR.Data(), runNumberDAQ2);
+  TFile *fout_L = new TFile(optFileName, "RECREATE");
   TTree *profile_L = new TTree("profile_L","profile_L");
   profile_L->Branch("ADCcount", ADC_L,      "ADCcount[32][2049]/I");
   profile_L->Branch("pedestal", pedestal_L, "pedestal[32]/D");
-  profile_L->Branch("RMS"     , RMS_L,      "RMS[32]/D");
+  profile_L->Branch("RMS",      RMS_L,      "RMS[32]/D");
   profile_L->Branch("integral", integral_L, "integral[32]/I");
-      profile_L->Branch("ientry",   &ientry_L,  "ientry/I");
+  profile_L->Branch("ientry",   &ientry_L,  "ientry/I");
 
-      // daqpc2
-      ientry_L = 0; 
-      while(!fin1[0].eof()) {
-	for(int channel=0; channel<CH; channel++) {
-	  Sum=0;pede=0;pedeRMS=0;
-	  for(int i=0;i<2;i++) {
-	    double dummy;
-	    fin1[channel]>>dummy;
-	  }
-	  for(int isamp=0; isamp<SAMPLE; isamp++) {
-	    fin1[channel]>>x;
-	    ADC_L[channel][isamp] = x;
-	    Sum += ADC_L[channel][isamp];
-	    if(isamp<100) {
-	      pede =  pede + ADC_L[channel][isamp];
-	      pedeRMS = pedeRMS + pow(ADC_L[channel][isamp],2.0)/100;
-	    }
-	  }
-	  integral_L[channel] = Sum; 
-	  pedestal_L[channel] = pede/100;
-	  RMS_L[channel] = sqrt(pedeRMS); //dataout(RMS_L[channel]);
-	}
-	profile_L->Fill(); cont++; ientry_L++;
+  // daqpc2
+  ientry_L = 0;
+  while (!inputDAQ2[0].eof()) {
+    for (int ich = 0; ich < CH; ich++) {
+      sumADC = 0;
+      pedestalValue = 0;
+      pedestalRMS   = 0;
+      for (int i = 0; i < 2; i++) {
+	double dummy;
+	inputDAQ2[ich] >> dummy;
       }
-        
-      ofn.Form("../profile/profile_daqpc2_run000%d.root", runNum1);
-      //TFile iu(ofn);
-      //profile_L->Write();
-      //iu.Close();
-      profile_L->SaveAs(ofn);
-      //profile_L->Write();
-      
-      TFile fi1(ofn);
-      int ADcount_L[CH][SAMPLE]; double peds_L[CH]; double rms_L[CH]; int integ_L[CH]; int ient_L;
-      TTree *tee = (TTree*)fi1.Get("profile_L");
-      tee->SetBranchAddress("ADCcount", ADcount_L);
-      tee->SetBranchAddress("pedestal", peds_L);
-      tee->SetBranchAddress("RMS"     , rms_L);
-      tee->SetBranchAddress("integral", integ_L);
-      tee->SetBranchAddress("ientry"  , &ient_L);
-
-      int nevt1 = tee->GetEntries();
-      cout << "nevt1 = " << nevt1 << "\n";
-      double realInt1[CH][nevt1];
-      TH2D *map1 = new TH2D("map1","map1",8,-12,12,8,-12,12);
-      TH2D **siArrayHist_L; siArrayHist_L = new TH2D*[nevt1];
-      for(int i=0;i<nevt1;i++) {
-	siArrayHist_L[i]=new TH2D(Form("h_L%d",i),"si array;x [mm];y [mm]",8,-12,12,8,-12,12);
-      }
-
-      for(int evt=0; evt<nevt1; evt++) {
-	tee->GetEntry(evt);
-	for(int ich=0; ich<CH; ich++) {
-	  realInt1[ich][evt] = integ_L[ich] - peds_L[ich]*SAMPLE;
-	  if(realInt1[ich][evt]<0) realInt1[ich][evt]=0;
-	}
-	for(int nch=0;nch<CH;nch++) {
-	  for(int s=0;s<realInt1[nch][evt]/nevt1;s++) {
-	    siArrayHist_L[evt]->Fill(xArray_L[nch],yArray_L[nch]);
-	    map1->Add(siArrayHist_L[evt],1);
-	    map_all->Add(siArrayHist_L[evt], 1);
-	    siArrayHist_L[evt]->Reset();
-	  }
+      for (int isample = 0; isample < SAMPLE; isample++) {
+	inputDAQ2[ich] >> x;
+	ADC_L[ich][isample] = x;
+	sumADC += ADC_L[ich][isample];
+	if (isample < 100) {
+	  pedestalValue = pedestalValue + ADC_L[ich][isample];
+	  pedestalRMS   = pedestalRMS + pow(ADC_L[ich][isample], 2.0) / 100;
 	}
       }
+      integral_L[ich] = sumADC;
+      pedestal_L[ich] = pedestalValue / 100;
+      RMS_L     [ich] = sqrt(pedestalRMS);
+    }
+    profile_L->Fill();
+    ientry_L++;
+  }
 
-      //map_all->Fit(g2);
+  // Write to TTree & Close
+  profile_L->Write();
+  fout_L->Close();
+  //
 
-      //map->Add(map1,1); //problem
-      TH1D *projectX_L = map1->ProjectionX("pjx1",-12,12);
-      TH1D *projectY_L = map1->ProjectionY("pjy1",-12,12);            
-    
-      //TString fileName = Form("output_%d_%d.root", runNum, runNum1);
-      TFile f2(fileName, "UPDATE");
-      f2.cd();
-      map1->Write();
-      map_all->Write();
-      c0->cd();
-      map_all->Draw("colz");
-      c0->Update();
-      app.Run();
-      //g2->Draw("same");
-      //g2->Write();
-      //siArrayHist_R->Write();
-      //map->Write();
-      //projectX_R->Write();
-      //projectY_R->Write();
-      projectX_L->Write();
-      projectY_L->Write();
-      f2.Clear();
+  TFile iptFile_L(optFileName);
+  int ADcount_L[CH][SAMPLE]; double peds_L[CH]; double rms_L[CH]; int integ_L[CH]; int ient_L;
+  TTree *tree_L = (TTree*)iptFile_L.Get("profile_L");
+  tree_L->SetBranchAddress("ADCcount", ADcount_L);
+  tree_L->SetBranchAddress("pedestal", peds_L);
+  tree_L->SetBranchAddress("RMS"     , rms_L);
+  tree_L->SetBranchAddress("integral", integ_L);
+  tree_L->SetBranchAddress("ientry"  , &ient_L);
+
+  int nevt_L = tree_L->GetEntries();
+  cout << "DAQPC2 event = " << nevt_L << "\n\n";
+  double realInt1[CH][nevt_L];
+  TH2D *map_L = new TH2D("map_L", "map_L", 8, -12, 12, 8, -12, 12);
+  TH2D **siArrayHist_L; siArrayHist_L = new TH2D*[nevt_L];
+  for (int i = 0; i < nevt_L; i++) siArrayHist_L[i] = new TH2D(Form("h_L%d",i),"si array;x [mm];y [mm]",8,-12,12,8,-12,12);
+
+  for (int evt = 0; evt < nevt_L; evt++) {
+    tree_L->GetEntry(evt);
+    for (int ich = 0; ich < CH; ich++) {
+      realInt1[ich][evt] = integ_L[ich] - peds_L[ich]*SAMPLE;
+      if (realInt1[ich][evt] < 0) realInt1[ich][evt]=0;
+    }
+    for (int nch = 0; nch < CH; nch++) {
+      for (int s = 0; s < realInt1[nch][evt]/nevt_L; s++) {
+	siArrayHist_L[evt]->Fill(xArray_L[nch], yArray_L[nch]);
+	map_L->Add(siArrayHist_L[evt], 1);
+	for (int i = 0; i < 2; i++) map_all[i]->Add(siArrayHist_L[evt], 1);
+	siArrayHist_L[evt]->Reset();
+      }
+    }
+  }
+
+  TH1D *projectX_L = map_L->ProjectionX("pjx_L", -12, 12);
+  TH1D *projectY_L = map_L->ProjectionY("pjy_L", -12, 12);
+  TH1D *projectX_all = map_all[0]->ProjectionX("pjx_all", -12, 12);
+  TH1D *projectY_all = map_all[0]->ProjectionY("pjy_all", -12, 12);
+
+  optFileName.Form("%soutput_%d_%d.root", OPTDIR.Data(), runNumberDAQ1, runNumberDAQ2);
+  TFile optFile(optFileName, "RECREATE");
+  optFile.cd();
+  map_R->Write();
+  projectX_R->Write();
+  projectY_R->Write();
+  map_L->Write();
+  projectX_L->Write();
+  projectY_L->Write();
+  map_all[0]->Write();
+  projectX_all->Write();
+  projectY_all->Write();
+  optFile.Close();
+
+  // for offline
+  TCanvas *c1 = new TCanvas("c1", "c1");
+  c1->cd();
+  map_all[0]->Draw("colz");
+  map_all[0]->Fit(g2);
+  TCanvas *c2 = new TCanvas("c2", "c2");
+  c2->cd();
+  map_all[1]->Draw("lego2z");
+
+  cout << "\n=============================================\n";
+  cout << "           Si Array Profile Result           \n";
+  cout << "=============================================\n";
+  cout << "\t   x    = " << g2->GetParameter(1) << " mm\n";
+  cout << "\t   y    = " << g2->GetParameter(3) << " mm\n";
+  cout << "\tsigma_x = " << g2->GetParameter(2) << " mm\n";
+  cout << "\tsigma_y = " << g2->GetParameter(4) << " mm\n";
+  cout << "=============================================\n\n";
+  //
+
+  // make backup data as pdf file
+  TString fileName = Form("%stest.pdf", OPTDIR.Data());
+  TCanvas *c5 = new TCanvas(fileName, fileName);
+  c5->Print(fileName + "[", "pdf");
+  map_R->Draw("colz");
+  c5->Print(fileName, "pdf");
+  projectX_R->Draw();
+  c5->Print(fileName, "pdf");
+  projectY_R->Draw();
+  c5->Print(fileName, "pdf");
+  map_L->Draw("colz");
+  c5->Print(fileName, "pdf");
+  projectX_L->Draw();
+  c5->Print(fileName, "pdf");
+  projectY_L->Draw();
+  c5->Print(fileName, "pdf");
+  map_all[0]->Draw("colz");
+  map_all[0]->Fit(g2, "Q");
+  //gStyle->SetOptStat(1000000001);
+  gStyle->SetOptFit(1);
+  c5->Print(fileName, "pdf");
+  map_all[1]->Draw("lego2z");
+  c5->Print(fileName, "pdf");
+  projectX_all->Draw();
+  c5->Print(fileName, "pdf");
+  projectY_all->Draw();
+  c5->Print(fileName, "pdf");
+  c5->Print(fileName + "]", "pdf");
+  delete c5;
+  //
+
+  cout << "\n... Back to terminal > Type 'ctl + C'\n";
+  app.Run();
 }
